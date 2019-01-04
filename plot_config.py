a6c8b27b0584b7ca94661d2f9d9a5eb31b17f216
Anton Osika <anton.osika@gmail.com>
2019-01-04 12:28:59 +0100

    Fix typo

--- plot_config.py
+++ plot_config.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pandas as pd
 import matplotlib as mpl
-import matplotlib.pytplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 mpl.style.use('seaborn-dark-palette')
